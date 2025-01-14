a59efa0a0434c2c0961aa9fbd8153c1ed0bc7dad
rinamakey <karaleva85@mail.ru>
2025-01-14 22:57:12 +0300

    first commit

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -10,13 +10,14 @@ const totalPrice = useSelector(getTotalPrice);
 
     return(
         <div>
-           
-            <span className="cart"> Корзина: 
-            
-            <img className="cartIcon" src="https://cdn-icons-png.freepik.com/256/806/806798.png?ga=GA1.1.1479483188.1702672119&semt=ais_hybrid"alt="foto"/>
-            </span>
+        
+            <span className="cart"> Корзина:  <img className="cartIcon" src="https://cdn-icons-png.freepik.com/256/806/806798.png?ga=GA1.1.1479483188.1702672119&semt=ais_hybrid"alt="foto"/> </span>
+    
+    
+        <div className="carttotal">
             <h3 className="total"> Общая сумма: {totalPrice} Pуб.</h3>
             {cartItems.map((cartItem,item) => <CartItem key={item} cartItem={cartItem}/> )}
+            </div>
         </div>
     )
 }
--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -12,7 +12,7 @@ const CartItem = ({cartItem}) => {
         <div>
             <h5>{dishes.name} - {cartItem.quantity} порция(и) </h5> 
 
-            <p>Цена {dishes.price* cartItem.quantity} руб.</p> 
+            <p >Цена {dishes.price* cartItem.quantity} руб.</p> 
 
             <span onClick={()=> dispatch(removeItemFromCart({cartItemId:cartItem.id}))}>
                 <img className="icon" src="https://cdn-icons-png.freepik.com/256/16973/16973571.png?ga=GA1.1.1479483188.1702672119&semt=ais_hybrid" alt="delete"/>
